bbf1a8b4c6b81ef3c6158fef401003413de012a9
pachotisab <pachotisab@myvuw.ac.nz>
2024-09-17 17:09:56 +1200

    final
    made the song shorter because i decided i didnt like how long it was, still over a min and a half. hope this is ok

--- music_starter.js
+++ music_starter.js
@@ -1,20 +1,27 @@
 let modenum;
 let facenum;
 let charanum = 50;
-let charaX = 350;
+let charaX = 300;
 let charaY = 150;
+let alp = 10;
+let boop = false;
+let beep = 7;
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   drawBg();
   
-
+ if(counter<=5000){
+    beep = 70;
+    charaX=300;
+    charaY = 200;
+  }
    
 if(counter<=3){
   modenum = 0;
   facenum = 1;
 }
 
-   if(counter%5==0){
+   if(counter%10==0){
     let = c = map(vocal,0,100,-90,90);
     let = d = map(drum,0,100,-90,90);
     let = o = map(other,0,100,185,360);
@@ -29,13 +36,15 @@ if(counter<=3){
    }
   
   
-   drawChara(modenum,facenum,0,charaX,charaY, c,0,-d,0, b,0,o,0, 0,0,0,0,0,7);
-   drawChara(modenum,facenum,1,charaX+450,charaY, c,0,-d,0, b,0,o,0, 0,0,0,0,0,7);
+   drawChara(modenum,facenum,0,charaX,charaY, c,0,-d,0, b,0,o,0, 0,0,0,0,0,beep);
+   drawChara(modenum,facenum,1,charaX+450,charaY, c,0,-d,0, b,0,o,0, 0,0,0,0,0,beep);
    strokeWeight(1);
-  
-   if(counter%300==0&&counter!=0){
+   if(counter<3000){
+
+   if(counter%100==0&&counter!=0){
     
     print(counter);
+    
       if(modenum==3){
         modenum=0;
       }else{
@@ -44,9 +53,10 @@ if(counter<=3){
       charaX += charanum;
    }
 
+
    
 
-   if(counter%300==0&&mode==3){
+   if(counter%100==0&&mode==3){
     print(counter);
       if(facenum==1){
         facenum = -1;
@@ -56,14 +66,54 @@ if(counter<=3){
         charanum = charanum*-1;
       }
    }
+  }else if(charaX<=300||boop == true){
+    boop=true;
+    charanum = 60;
+    if(counter%100==0&&counter!=0){
+    
+      print(counter);
+      
+        if(modenum==0){
+          modenum=3;
+        }else{
+          modenum = modenum-1;
+        }  
+        charaX += charanum;
+     }
+  
+  
+     
+  
+     if(counter%100==0&&mode==3){
+      print(counter);
+        if(facenum==1){
+          facenum = -1;
+          charanum = charanum*-1;
+        }else{
+          facenum = 1;
+          charanum = charanum*-1;
+        }
+     }
+
+
+  }
    print(counter);
-   // display "words"
-   stroke(colorArray[1]);
-  fill(colorArray[0]);
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+ 
 
    drawFg();
+
+   alp = 100-((vocal+drum+bass)/3);
+   let alpha = map(alp,0,100,5,120);
+
+   noStroke();
+   fill(27, 0, 110,alpha);
+   rect(0,0,width,height);
+
+     // display "words"
+     stroke(colorArray[1]);
+     fill(colorArray[0]);
+      textAlign(CENTER);
+      textSize(vocal/2);
+      text(words, width/2, height*2.5/3);
 }
 
